58580004fe68b7c96b2b852914fe904746e9a648
james <jan31@my.bcit.ca>
2023-04-01 13:20:36 -0700

    set up search/filter from lab

--- assignment3/src/App.js
+++ assignment3/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Search from "./Search";
+import FilteredPokemons from "./FilteredPokemons";
+import { useState } from "react";
 
 function App() {
+  const [typeSelectedArray, setTypeSelectedArray] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Search
+        setTypeSelectedArray={setTypeSelectedArray}
+        typeSelectedArray={typeSelectedArray}
+      />
+      <FilteredPokemons
+        typeSelectedArray={typeSelectedArray}
+      />
+
+    </>
   );
 }
 
--- assignment3/src/FilteredPokemons.js
+++ assignment3/src/FilteredPokemons.js
@@ -2,37 +2,38 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import axios from 'axios'
 
-function FilteredPokemons() {
-    const [pokemons, setPokemons] = useState([])
+function FilteredPokemons({ typeSelectedArray }) {
 
-    useEffect(() => {
-        async function fetchPokemons() {
-            const response = await axios.get('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json')
-            setPokemons(response.data.pokemon)
-        }
-        fetchPokemons()
-    }, [])
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    async function fetchPokemons() {
+      const response = await axios.get('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json')
+      setPokemons(response.data)
+    }
+    fetchPokemons()
+  }, [])
 
 
-    return (
-        <div>
-            {
-                pokemons.map(pokemon => {
-                    if (pokemon.type.some(type => typeSelectedArray.includes(type)))
-                        return (
-                            <div key={pokemon.id}>
-                                {pokemon.name.english}
-                            </div>
-                        )
-                // <div key={pokemon.id}>
-                //     pokemon.name.english
-                // </div>
-                // )
-                })
-            }
-        </div>
-    )
+  return (
+    <div>
+      {
+        pokemons.map(pokemon => {
+          if (typeSelectedArray.every(type => pokemon.type.includes(type))) {
+            return (
+              <div key={pokemon.id}>
+                {pokemon.name.english}
+                <ul>
+                  {pokemon.type.map(type => <li key={type}>{type}</li>)}
+                </ul>
+              </div>)
+          }
+        })
+      }
+    </div>
+  )
 
 }
+
 
 export default FilteredPokemons
